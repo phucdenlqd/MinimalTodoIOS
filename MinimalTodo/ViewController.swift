2555468a2b4777fb94b88a95d7f5aa80aad67faa
Phuc Nom <phucdenlqd@icloud.com>
2019-01-01 03:39:39 -0800

    Initial Commit

---
+++ MinimalTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinimalTodo
+//
+//  Created by Phuc Nguyen on 01/01/2019.
+//  Copyright © 2019 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
