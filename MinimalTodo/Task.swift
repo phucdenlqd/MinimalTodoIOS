47f3b26317f1d544f661c27568c0d4ceb8a52ccb
PhucNguyen <phucdenlqd@gmail.com>
2019-01-01 04:34:48 -0800

    add Task

--- MinimalTodo/Task.swift
+++ MinimalTodo/Task.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+public class Task{
+    var id:Int
+    var title : String
+    var date : String
+    init(title:String,date:String){
+        self.id=0
+        self.title=title
+        self.date=date
+    }
+    init(id:Int,title:String,date:String){
+        self.id=id
+        self.title=title
+        self.date=date
+    }
+    func getTitle() -> String {
+        return self.title
+    }
+    func getDate() -> String {
+        return self.date
+    }
+    func getId() -> Int {
+        return self.id
+    }
+}
